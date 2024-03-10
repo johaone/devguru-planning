011efff221527305718b37d95b71b8d9157c5075
johaone <johaone_7.7@mail.ru>
2024-03-10 13:56:12 +0300

    fix maven structure

--- planner-server/src/main/java/ru/devguru/springms/planner/server/PlannerServerApplication.java
+++ planner-server/src/main/java/ru/devguru/springms/planner/server/PlannerServerApplication.java
@@ -1,5 +1,4 @@
 package ru.devguru.springms.planner.server;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
